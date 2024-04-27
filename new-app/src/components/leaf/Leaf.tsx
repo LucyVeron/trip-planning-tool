86953b078b4f8dec3bf8afc650769b0775a8df19
Lucy Veron <lucymveron@gmail.com>
2024-04-27 13:03:39 +0200

    setting up leaflet map

---
+++ new-app/src/components/leaf/Leaf.tsx
@@ -0,0 +1,8 @@
+import { MapContainer } from "react-leaflet";
+import "./Leaf.css";
+
+function Leaf() {
+  return <MapContainer center={[48.864716, 2.349014]}></MapContainer>;
+}
+
+export default Leaf;
