27593594e37f192a0f224693e35fe4670df72643
Lucy Veron <lucymveron@gmail.com>
2024-04-27 14:10:00 +0200

    trying some markers

--- new-app/src/components/leaf/Leaf.tsx
+++ new-app/src/components/leaf/Leaf.tsx
@@ -1,8 +1,45 @@
-import { MapContainer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import "./Leaf.css";
+import { Icon } from "leaflet";
 
 function Leaf() {
-  return <MapContainer center={[48.864716, 2.349014]}></MapContainer>;
+  const markers = [
+    {
+      geocode: [48.86, 2.3522],
+      popUp: "Hello, I am pop up 1",
+    },
+    {
+      geocode: [48.85, 2.3522],
+      popUp: "Hello, I am pop up 2",
+    },
+    {
+      geocode: [48.855, 2.34],
+      popUp: "Hello, I am pop up 3",
+    },
+  ];
+
+  const customIcon = new Icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+    iconSize: [38, 38],
+  });
+
+  return (
+    <MapContainer center={[48.864716, 2.349014]} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {markers.map((marker) => (
+        <Marker position={marker.geocode as any} icon={customIcon}>
+          <Popup>
+            <h2>{marker.popUp}</h2>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
 }
 
 export default Leaf;
