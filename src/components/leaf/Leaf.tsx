f401607f09a42112a665f425098232659e422da3
Lucy Veron <lucymveron@gmail.com>
2024-05-01 12:44:07 +0200

    click to set marker

--- src/components/leaf/Leaf.tsx
+++ src/components/leaf/Leaf.tsx
@@ -1,43 +1,44 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./Leaf.css";
 import { Icon } from "leaflet";
+import { useState } from "react";
 
-function Leaf() {
-  const markers = [
-    {
-      geocode: [48.86, 2.3522],
-      popUp: "Hello, I am pop up 1",
-    },
-    {
-      geocode: [48.85, 2.3522],
-      popUp: "Hello, I am pop up 2",
-    },
-    {
-      geocode: [48.855, 2.34],
-      popUp: "Hello, I am pop up 3",
-    },
-  ];
-
+function LocationMarker() {
   const customIcon = new Icon({
     iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
     iconSize: [38, 38],
   });
+  const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    click: (e: any) => {
+      map.locate();
+      // map.flyTo(e.latlng, map.getZoom());
+      setPosition(e.latlng);
+    }
+  });
 
+  return position === null ? null : (
+    <Marker position={position} icon={customIcon}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+}
+
+function Leaf() {
   return (
-    <MapContainer center={[48.864716, 2.349014]} zoom={13}>
+    <MapContainer center={[48.864716, 2.349014]} zoom={11}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-
-      {markers.map((marker) => (
-        <Marker position={marker.geocode as any} icon={customIcon}>
-          <Popup>
-            <h2>{marker.popUp}</h2>
-          </Popup>
-        </Marker>
-      ))}
+      <LocationMarker />
     </MapContainer>
   );
 }
