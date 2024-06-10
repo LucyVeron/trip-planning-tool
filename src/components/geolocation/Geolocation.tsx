f50a8d2f6b17d1c1043ebb52651fe1c00d0433a0
Lucy Veron <lucymveron@gmail.com>
2024-06-10 21:17:33 +0200

    show lat and lon

--- src/components/geolocation/Geolocation.tsx
+++ src/components/geolocation/Geolocation.tsx
@@ -1,13 +1,29 @@
 import "./Geolocation.css";
 import { useGeolocation } from "../../hooks/useGeolocation";
 
+interface Geo {
+  locationInfo: GeolocationCoordinates | null;
+  locationError: any;
+}
+
 function Geolocation() {
-  const { locationInfo, locationError } = useGeolocation();
-  console.error(locationInfo, locationError);
+  const loc = useGeolocation() as Geo;
+
   return (
     <div>
-      <h1>Your Location</h1>
-      <p>{locationInfo && (locationInfo as any).latitude}</p>
+      <h1>Your Location:</h1>
+      {loc?.locationInfo ? (
+        <>
+          <div>
+            <b>Lat:</b> {loc.locationInfo.latitude}
+          </div>
+          <div>
+            <b>Lon:</b> {loc.locationInfo.longitude}
+          </div>
+        </>
+      ) : (
+        "loading..."
+      )}
     </div>
   );
 }
--- src/hooks/useGeolocation.js
+++ src/hooks/useGeolocation.js
@@ -1,23 +1,21 @@
 import { useState } from "react";
 
 export const useGeolocation = () => {
-    const [locationInfo, setLocationInfo] = useState(null);
-    const [locationError, setLocationError] = useState(null);
-    const { geolocation } = navigator;
+  const [locationInfo, setLocationInfo] = useState(null);
+  const [locationError, setLocationError] = useState(null);
+  const { geolocation } = navigator;
 
-    const successFn = (res) => {
-        console.log({ res });
-        setLocationInfo(res.coords);
-    }
+  const successFn = (res) => {
+    setLocationInfo(res.coords);
+  };
 
-    const errorFn = (res) => {
-        console.log({ res });
-        setLocationError(res.message);
-    }
+  const errorFn = (res) => {
+    setLocationError(res.message);
+  };
 
-    if (!locationError && !locationInfo) {
-        geolocation.getCurrentPosition(successFn, errorFn);
-    }
+  if (!locationError && !locationInfo) {
+    geolocation.getCurrentPosition(successFn, errorFn);
+  }
 
-    return { locationError, locationInfo }
-}
+  return { locationError, locationInfo };
+};
